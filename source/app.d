f640078a9432124074063c4663461b62171eec8e
Thomas Gann <thomasleegann@gmail.com>
2014-12-23 20:08:26 -0500

    Documented app.d

--- source/app.d
+++ source/app.d
@@ -3,6 +3,10 @@ import std.algorithm;
 import std.range;
 import dborg.bot;
 
+//==============================================================
+//Returns the value of the given command line parameter (defined
+//as the parm directly following the one that matches 'name').
+//==============================================================
 string getParm(string args[], string name)
 {
     foreach(a; 1 .. args.length - 1)
@@ -14,11 +18,18 @@ string getParm(string args[], string name)
     return null;
 }
 
+//=============================================================
+//Returns true if the given parameter is in the parameter list.
+//=============================================================
 bool getSwitch(string args[], string name)
 {
     return args[1 .. $].canFind(name);
 }
 
+//=====================================================================
+//Loads a library of lines into the given bot, with the given filename.
+//Returns true on success, false upon failure.
+//=====================================================================
 bool loadLines(DBorg bot, string fileName)
 {
     //Open the file.
@@ -35,6 +46,7 @@ bool loadLines(DBorg bot, string fileName)
 
     string line;
 
+    //Learn each line in the file.
     while((line = botFile.readln()) !is null)
         bot.learn(line);
 
@@ -42,6 +54,10 @@ bool loadLines(DBorg bot, string fileName)
     return true;
 }
 
+//===================================================================
+//Saves the bot's known lines as a text file with the given filename.
+//Returns true on success, false upon failure.
+//===================================================================
 bool saveLines(DBorg bot, string fileName)
 {
     File outFile;
@@ -55,6 +71,7 @@ bool saveLines(DBorg bot, string fileName)
         return false;
     }
 
+    //Save each sentence as its own line in the file.
     foreach(sentence; bot.sentences)
          outFile.writeln(sentence.joiner(" ").array);
 
@@ -66,6 +83,7 @@ void main(string args[])
 {
     auto bot = new DBorg;
 
+    //Read through the command line parameters.
     string paramTopic = getParm(args, "--topic");
     string paramReply = getParm(args, "--reply");
     string fileName = getParm(args, "--file");
@@ -73,23 +91,29 @@ void main(string args[])
     bool learning = !getSwitch(args, "--no-learning");
     bool saveEnabled = getSwitch(args, "--save-lines");
 
+    //Default to 'lines.txt' for a library file if none is specified.
     if(fileName is null)
         fileName = "lines.txt";
 
     bot.loadLines(fileName);
-
+    
+    //The 'topic' parameter forces the bot to talk about the given topic
+    //and immediately terminate.
     if(paramTopic !is null)
     {
         writeln(bot.sayTopic(paramTopic));
         return;
     }
 
+    //The 'reply' parameter forces the bot to reply to the given sentence,
+    //and then immediately terminate.
     if(paramReply !is null)
     {
         writeln(bot.reply(paramReply));
         return;
     }
 
+    //The 'stats' parameter displays statistics about the bot's dictionary.
     if(statMode)
     {
         writefln("%d words", bot.words.length);
@@ -98,6 +122,7 @@ void main(string args[])
         return;
     }
 
+    //Main loop for interactively chatting with the bot.
     while(true)
     {
         write("<You> ");
